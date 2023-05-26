0e0633bfa44afe3efb4fa06e6aa0538d9710ff4f
phant80m <black.pandazayd@gmail.com>
2023-05-26 18:59:26 +1000

    animations, js and more!

---
+++ app.js
@@ -0,0 +1,51 @@
+  $(document).ready(function() {
+    $("#docs").hover(
+      function() {
+        $("#document-icon").addClass("fa-bounce");
+      },
+      function() {
+        $("#document-icon").removeClass("fa-bounce");
+      }
+    );
+  });
+  $(document).ready(function() {
+    $("#discord").hover(
+      function() {
+        $("#discord-icon").addClass("fa-shake");
+      },
+      function() {
+        $("#discord-icon").removeClass("fa-shake");
+      }
+    );
+  });
+  $(document).ready(function() {
+    $("#github-1").hover(
+      function() {
+        $("#github-icon").addClass("fa-beat");
+      },
+      function() {
+        $("#github-icon").removeClass("fa-beat");
+      }
+    );
+  });
+  $(document).ready(function() {
+    $("#github-1").hover(
+      function() {
+        $("#github-icon").addClass("fa-beat");
+      },
+      function() {
+        $("#github-icon").removeClass("fa-beat");
+      }
+    );
+  });
+  $(document).ready(function() {
+    $("#logo-header").hover(
+      function() {
+        $("#logo-header").addClass("fa-bounce");
+      },
+      function() {
+        $("#logo-header").removeClass("fa-bounce");
+      }
+    );
+  });
+
