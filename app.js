9806b82a5586703cb28c0cc4d1a683515e7cb3bd
phant80m <black.pandazayd@gmail.com>
2023-06-03 11:05:24 +1000

    subtle glow fx

--- app.js
+++ app.js
@@ -1,70 +1,64 @@
-  $(document).ready(function() {
-    $("#docs").hover(
-      function() {
-        $("#document-icon").addClass("fa-bounce");
-      },
-      function() {
-        $("#document-icon").removeClass("fa-bounce");
-      }
-    );
+$(document).ready(function() {
+  $("#docs").hover(function() {
+    $("#document-icon").toggleClass("fa-bounce");
+    $("#document-icon").toggleClass("glow-orange")
   });
-  $(document).ready(function() {
-    $("#discord").hover(
-      function() {
-        $("#discord-icon").addClass("fa-beat");
-      },
-      function() {
-        $("#discord-icon").removeClass("fa-beat");
-      }
-    );
+
+  $("#discord").hover(function() {
+    $("#discord-icon").toggleClass("fa-beat");
   });
-  $(document).ready(function() {
-    $("#github-1").hover(
-      function() {
-        $("#github-icon").addClass("fa-beat");
-      },
-      function() {
-        $("#github-icon").removeClass("fa-beat");
-      }
-    );
+
+  $("#github-1").hover(function() {
+    $("#github-icon").toggleClass("fa-beat");
   });
-  $(document).ready(function() {
-    $("#github-1").hover(
-      function() {
-        $("#github-icon").addClass("fa-beat");
-      },
-      function() {
-        $("#github-icon").removeClass("fa-beat");
-      }
-    );
-  });
-  $(document).ready(function() {
-    $("#logo-header").hover(
-      function() {
-        $("#logo-header").addClass("fa-bounce");
-      },
-      function() {
-        $("#logo-header").removeClass("fa-bounce");
-      }
-    );
+
+  $("#logo-header").hover(function() {
+    $(this).toggleClass("fa-bounce");
   });
 
   $(document).ready(function() {
-    function handleScreenResize() {
-      var discordIcon = $("#document-icon");
-      if ($(window).width() <= 720) {
-        discordIcon.addClass("");
-      } else {
-        discordIcon.removeClass("");
-      }
-    }
-
-    // Call the function on page load
-    handleScreenResize();
-
-    // Call the function when the window is resized
-    $(window).resize(handleScreenResize);
-  });
- window.addEventListener("DOMContentLoaded", function() {
-      document.body.style.opacity = "1";
+    $(".logo-text").hover(function() {
+      $(this).toggleClass("glow-orange-text");
+      $(".logo-header").toggleClass("glow-orange");
     });
+  });
+  
+  $(document).ready(function() {
+    $(".header").hover(function() {
+      $(".header-container::after").toggleClass("glow-orange");
+    });
+  });
+
+$(document).ready(function() {
+  $("#discord").hover(function() {
+    $("#discord-icon").toggleClass("glow-blue");
+  });
+
+  $("#github-1").hover(function() {
+    $("#github-icon").toggleClass("glow-white");
+  });
+});
+
+  
+  function handleScreenResize() {
+    var discordIcon = $("#document-icon");
+    if ($(window).width() <= 720) {
+      discordIcon.addClass("");
+    } else {
+      discordIcon.removeClass("");
+    }
+  }
+
+
+  handleScreenResize();
+
+  $(window).resize(handleScreenResize);
+});
+
+window.addEventListener("DOMContentLoaded", function() {
+  document.body.style.opacity = "1";
+});
+
+function handleUnimplemented() {
+  alert("This feature is not yet available, sorry for the inconvenience");
+}
